1f75f4ac3f62566a3e34f6aee95dacfe85bcfb43
IhorMtr <igormotornyyy@gmail.com>
2026-02-13 21:35:21 +0200

    feat: update allowedOrigins to include production URL

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,6 @@
-export const allowedOrigins = ['http://localhost:3000'];
+export const allowedOrigins = [
+  'http://localhost:3000',
+  'https://practice-fe-iota.vercel.app',
+];
 
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
